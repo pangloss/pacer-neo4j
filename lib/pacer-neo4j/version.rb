52bdd0ef791020c0db347923bf38fa6024402814
Darrick Wiebe <dw@xnlogic.com>
2012-08-21 18:14:12 -0400

    Bumped maven deps to release versions

--- lib/pacer-neo4j/version.rb
+++ lib/pacer-neo4j/version.rb
@@ -3,7 +3,7 @@ module Pacer
     VERSION = "2.0.0.pre"
     JAR = "pacer-neo4j-#{ VERSION }-standalone.jar"
     JAR_PATH = "lib/#{ JAR }"
-    BLUEPRINTS_VERSION = "2.1.0-SNAPSHOT"
-    PIPES_VERSION = "2.1.0-SNAPSHOT"
+    BLUEPRINTS_VERSION = "2.1.0"
+    PIPES_VERSION = "2.1.0"
   end
 end
