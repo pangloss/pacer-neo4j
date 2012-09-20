9790a2047ebc47dd8c9e004314360201f37229ab
Darrick Wiebe <dw@xnlogic.com>
2012-09-20 01:47:25 -0400

    Fix loading outside of rubygems

--- lib/pacer-neo4j.rb
+++ lib/pacer-neo4j.rb
@@ -1,6 +1,10 @@
+require 'pacer' unless defined? Pacer
+
+lib_path = File.expand_path(File.join(File.dirname(__FILE__), '../lib'))
+$:.unshift lib_path unless $:.any? { |path| path == lib_path }
+
 require 'pacer-neo4j/version'
 
-require 'pacer'
 require Pacer::Neo4j::JAR
 
 require 'pacer-neo4j/graph'
