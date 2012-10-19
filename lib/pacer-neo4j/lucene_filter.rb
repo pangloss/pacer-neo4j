bc6d55af8926de29dadbe9e51d77bae75d14aea3
Darrick Wiebe <dw@xnlogic.com>
2012-10-19 15:30:43 -0400

    Support Lucene Queries' various options.

--- lib/pacer-neo4j/lucene_filter.rb
+++ lib/pacer-neo4j/lucene_filter.rb
@@ -1,10 +1,12 @@
 module Pacer
   module Filter
     module LuceneFilter
-      attr_accessor :index, :query
+      import org.neo4j.index.lucene.QueryContext
+
+      attr_accessor :index, :query, :sort_by, :reverse_numeric, :sort_numeric, :sort_by_score, :top, :fast
 
       def count(max = nil)
-        iter = result
+        iter = query_result
         c = iter.count
         if c >= 0
           c
@@ -23,21 +25,58 @@ module Pacer
         iter.close
       end
 
+      def sort_by_score!
+        self.sort_by_score = true
+        self
+      end
+
+      def sort(*keys)
+        self.sort_by = keys
+        self
+      end
+
+      def top_hits(n)
+        self.top = n
+        self
+      end
+
+      def fast!
+        self.fast = true
+        self
+      end
+
       protected
 
-      def result
-        index.query query
+      def build_query
+        qc = QueryContext.new(query)
+        qc = qc.tradeCorrectnessForSpeed if fast
+        qc = qc.top(top) if top
+        if sort_by_score
+          qc.sortByScore
+        elsif sort_by
+          qc.sort(*[*sort_by].map(&:to_s))
+        elsif sort_numeric
+          qc.sortNumeric(sort_numeric, false)
+        elsif reverse_numeric
+          qc.sortNumeric(reverse_numeric, true)
+        else
+          qc
+        end
+      end
+
+      def query_result
+        index.query build_query
       end
 
       def source_iterator
         pipe = Pacer::Neo4j::RawVertexWrappingPipe.new graph
-        pipe.setStarts result
+        pipe.setStarts query_result
         pipe.enablePath(true)
         pipe
       end
 
       def inspect_string
-        "#{ inspect_class_name }(#{ query }) ~ #{ result.count }"
+        "#{ inspect_class_name }(#{ query }) ~ #{ query_result.count }"
       end
     end
   end
