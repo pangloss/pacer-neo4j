85501ede7d7c8bf62b131d56431ad6ed51b33af9
Darrick Wiebe <dw@xnlogic.com>
2012-11-18 02:48:31 -0500

    slowly figuring out what needs to be quoted in queries

--- lib/pacer-neo4j/graph.rb
+++ lib/pacer-neo4j/graph.rb
@@ -40,8 +40,8 @@ module Pacer
               "#{k}:#{v}"
             else
               s = encode_property(v)
-              if s.is_a? String and s =~ /\s/
-                %{#{k}:"#{s}"}
+              if s.is_a? String and s =~ /[\t :"']/
+                %{#{k}:#{s.inspect}}
               else
                 "#{k}:#{s}"
               end
