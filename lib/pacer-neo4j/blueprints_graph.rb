b04a860df2326926b429246336a8735de074af0f
Darrick Wiebe <dw@xnlogic.com>
2012-11-21 04:11:03 -0500

    handle nested transactions.

--- lib/pacer-neo4j/blueprints_graph.rb
+++ lib/pacer-neo4j/blueprints_graph.rb
@@ -2,16 +2,22 @@ module Pacer
   module Neo4j
     class BlueprintsGraph < com.tinkerpop.blueprints.impls.neo4j.Neo4jGraph
       attr_accessor :allow_auto_tx
+      attr_reader :tx_depth
+
+      def initialize(*args)
+        super
+        @tx_depth = 0
+      end
 
       def transaction
-        @allow_auto_tx = true
+        @tx_depth += 1
         yield
       ensure
-        @allow_auto_tx = false
+        @tx_depth -= 1
       end
 
       def autoStartTransaction
-        if allow_auto_tx
+        if tx_depth != 0 or allow_auto_tx
           super
         else
           raise Pacer::TransactionError, "Can't mutate the graph outside a transaction block"
