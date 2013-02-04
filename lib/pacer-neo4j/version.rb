a584be340013e03fa84636e1d37fc6ef1948af8f
Darrick Wiebe <dw@xnlogic.com>
2013-02-04 17:33:57 -0500

    Revert blueprints version temporarily

--- lib/pacer-neo4j/version.rb
+++ lib/pacer-neo4j/version.rb
@@ -1,10 +1,10 @@
 module Pacer
   module Neo4j
-    VERSION = "2.1.1"
+    VERSION = "2.1.2"
     JAR = "pacer-neo4j-#{ VERSION }-standalone.jar"
     JAR_PATH = "lib/#{ JAR }"
-    BLUEPRINTS_VERSION = "2.2.0"
-    PIPES_VERSION = "2.2.0"
+    BLUEPRINTS_VERSION = "2.1.0"
+    PIPES_VERSION = "2.1.0"
     PACER_REQ = ">= 1.1.2"
   end
 end
