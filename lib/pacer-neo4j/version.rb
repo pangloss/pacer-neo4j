57389534746dc9ffaeda5ffc3a749fd1554472ed
Tasveer Singh <taz@tazsingh.com>
2013-08-23 12:29:14 -0400

    Upgrade Blueprints and Pipes

--- lib/pacer-neo4j/version.rb
+++ lib/pacer-neo4j/version.rb
@@ -3,8 +3,8 @@ module Pacer
     VERSION = "2.1.4"
     JAR = "pacer-neo4j-#{ VERSION }-standalone.jar"
     JAR_PATH = "lib/#{ JAR }"
-    BLUEPRINTS_VERSION = "2.3.0"
-    PIPES_VERSION = "2.3.0"
+    BLUEPRINTS_VERSION = "2.4.0"
+    PIPES_VERSION = "2.4.0"
     PACER_REQ = ">= 1.1.2"
   end
 end
