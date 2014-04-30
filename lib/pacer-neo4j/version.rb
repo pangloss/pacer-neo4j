322f6bf59d2a04f535441a8aa0d39b4d741320f7
Darrick Wiebe <dw@xnlogic.com>
2014-04-30 14:20:16 -0400

    Update to use released Tinkerpop jars

--- lib/pacer-neo4j/version.rb
+++ lib/pacer-neo4j/version.rb
@@ -1,10 +1,10 @@
 module Pacer
   module Neo4j
-    VERSION = "2.2.0.pre"
+    VERSION = "2.2.0"
     JAR = "pacer-neo4j-#{ VERSION }-standalone.jar"
     JAR_PATH = "lib/#{ JAR }"
-    BLUEPRINTS_VERSION = "2.5.0-SNAPSHOT"
-    PIPES_VERSION = "2.5.0-SNAPSHOT"
-    PACER_REQ = ">= 1.5.0.pre"
+    BLUEPRINTS_VERSION = "2.5.0"
+    PIPES_VERSION = "2.5.0"
+    PACER_REQ = ">= 1.5.0"
   end
 end
