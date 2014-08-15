107e46aeeebf6040cdb14537b6d56acecfa7a932
Darrick Wiebe <dw@xnlogic.com>
2014-08-15 13:58:43 -0400

    bump to stable blueprints

--- lib/pacer-neo4j/version.rb
+++ lib/pacer-neo4j/version.rb
@@ -3,8 +3,8 @@ module Pacer
     VERSION = "3.0.0.pre"
     JAR = "pacer-neo4j-#{ VERSION }-standalone.jar"
     JAR_PATH = "lib/#{ JAR }"
-    BLUEPRINTS_VERSION = "2.5.0-SNAPSHOT"
-    PIPES_VERSION = "2.5.0-SNAPSHOT"
+    BLUEPRINTS_VERSION = "2.5.0"
+    PIPES_VERSION = "2.5.0"
     PACER_REQ = ">= 1.5.0.pre"
   end
 end
