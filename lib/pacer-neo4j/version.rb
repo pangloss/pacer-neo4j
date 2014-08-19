1951d8c08e5d1c9bf458e12d3289550a849903c2
Darrick Wiebe <dw@xnlogic.com>
2014-08-19 16:57:42 -0400

    Need a fix from Blueprints 2.6

--- lib/pacer-neo4j/version.rb
+++ lib/pacer-neo4j/version.rb
@@ -3,8 +3,7 @@ module Pacer
     VERSION = "2.2.0"
     JAR = "pacer-neo4j-#{ VERSION }-standalone.jar"
     JAR_PATH = "lib/#{ JAR }"
-    BLUEPRINTS_VERSION = "2.5.0"
-    PIPES_VERSION = "2.5.0"
+    BLUEPRINTS_VERSION = "2.6.0-SNAPSHOT"
     PACER_REQ = ">= 1.5.0"
   end
 end
