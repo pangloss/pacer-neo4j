f421479f26dbf6e207e87214bdda0d6e88c143ff
Darrick Wiebe <dw@xnlogic.com>
2014-11-18 17:31:58 -0500

    Just register my Jarfile

--- lib/pacer-neo4j.rb
+++ lib/pacer-neo4j.rb
@@ -5,10 +5,6 @@ $:.unshift lib_path unless $:.any? { |path| path == lib_path }
 
 require 'pacer-neo4j/version'
 
-require 'lock_jar'
-LockJar.lock(File.join(File.dirname(__FILE__), "..", "Jarfile"))
-LockJar.load
-
 require 'pacer-neo4j/graph'
 require 'pacer-neo4j/algo/wrapping'
 require 'pacer-neo4j/algo/path_pipe'
--- lib/pacer-neo4j/version.rb
+++ lib/pacer-neo4j/version.rb
@@ -1,6 +1,9 @@
 module Pacer
   module Neo4j
-    VERSION = "2.3.1.pre"
-    PACER_REQ = ">= 2.0.0.pre"
+    VERSION = "2.3.2.pre"
+    PACER_REQ = ">= 2.0.4.pre"
+    if defined? Pacer::JARFILES
+      Pacer::JARFILES << File.join(File.dirname(__FILE__), "../..", "Jarfile")
+    end
   end
 end
