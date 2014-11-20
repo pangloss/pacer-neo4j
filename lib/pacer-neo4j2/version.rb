981066692b894d551ae5937b5f40f6fc849e9577
Darrick Wiebe <dw@xnlogic.com>
2014-11-20 17:09:46 -0500

    Simple LockJar usage. Combination is taken care of externally

--- lib/pacer-neo4j2/version.rb
+++ lib/pacer-neo4j2/version.rb
@@ -1,9 +1,6 @@
 module Pacer
   module Neo4j2
     VERSION = "2.1.6.pre"
-    PACER_REQ = ">= 2.0.4.pre"
-    if defined? Pacer::JARFILES
-      Pacer::JARFILES << File.join(File.dirname(__FILE__), "../..", "Jarfile")
-    end
+    PACER_REQ = ">= 2.0.5.pre"
   end
 end
