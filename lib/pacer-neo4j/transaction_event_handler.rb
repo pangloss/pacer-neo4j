75dab51c398978acd189b823b995a9a2c0980c29
Darrick Wiebe <dw@xnlogic.com>
2016-02-11 00:35:03 -0500

    Updated tx handlers

--- lib/pacer-neo4j/transaction_event_handler.rb
+++ lib/pacer-neo4j/transaction_event_handler.rb
@@ -3,27 +3,52 @@ module Pacer
     class TransactionEventHandler
       include org.neo4j.graphdb.event.TransactionEventHandler
 
-      attr_reader :graph
-      attr_accessor :on_commit, :on_commit_failed, :before_commit
+      attr_reader :graph, :enable_cache
+      attr_accessor :on_commit         , :on_commit_failed         , :before_commit
+      attr_accessor :on_commit_wrapper , :on_commit_failed_wrapper , :before_commit_wrapper
 
       def initialize(graph)
         @graph = graph
+        on_commit_wrapper = on_commit_failed_wrapper = before_commit_wrapper = TxDataWrapper
       end
 
       def unregister!
         graph.drop_handler self
       end
 
+      def enable_cache!
+        @enable_cache = true
+      end
+
       private
 
       # Return value is passed to afterCommit or afterRollback, but some values can cause crashes.
       def beforeCommit(data)
-        before_commit.call TxDataWrapper.new data, graph if before_commit
-        nil
+        if before_commit_wrapper and (before_commit or enable_cache)
+          wrapped = before_commit_wrapper.new data, graph
+        end
+        if before_commit
+          if before_commit_wrapper
+            before_commit.call wrapped
+          else
+            before_commit.call
+          end
+        end
+        if enable_cache
+          wrapped.data
+        end
       end
 
-      def afterCommit(data, ignore)
-        on_commit.call TxDataWrapper.new data, graph if on_commit
+      def afterCommit(data, cache)
+        if on_commit
+          if cache
+            on_commit.call cache
+          elsif on_commit_wrapper
+            on_commit.call on_commit_wrapper.new data, graph
+          else
+            on_commit.call
+          end
+        end
       end
 
       # This is actually only called if the commit fails and then it internally tries to
@@ -32,8 +57,16 @@ module Pacer
       # An exception in beforeCommit can definitely trigger this.
       #
       # Regular rollbacks do not get seen by the transaction system and no callback happens.
-      def afterRollback(data, ignore)
-        on_commit_failed.call TxDataWrapper.new data, graph if on_commit_failed
+      def afterRollback(data, cache)
+        if on_commit_failed
+          if cache
+            on_commit_failed.call cache
+          elsif on_commit_failed_wrapper
+            on_commit_failed.call on_commit_failed_wrapper.new data, graph
+          else
+            on_commit_failed.call
+          end
+        end
       end
     end
   end
